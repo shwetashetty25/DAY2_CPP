7c83b08ae72559a750d583cba6ec0ab0ab25e7fe
Shweta Shetty <shettyshweta48@gmail.com>
2024-12-11 10:09:20 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int integer = 5;
+    float floatingPoint = 5.99;
+    double doublePrecision = 9.98;
+    char character = 'D';
+    bool boolean = true;
+    string str = "Hello";
+
+    cout << "int : " << integer << endl;
+    cout << "float : " << floatingPoint << endl;
+    cout << "double : " << doublePrecision << endl;
+    cout << "char : " << character << endl;
+    cout << "bool : " << boolean << endl;
+    cout << "string : " << str << endl;
+
+    return 0;
+}
